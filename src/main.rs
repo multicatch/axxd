8f65408b9e3161b49226da84ef030b00c0c3e3c9
multicatch <cezaryre@gmail.com>
2021-07-23 00:09:44 +0200

    Added basic key derive function and guid slicing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
+use crypto::pbkdf2::pbkdf2;
+use crypto::hmac::Hmac;
+use crypto::sha1::Sha1;
+use crypto::digest::Digest;
+use std::path::PathBuf;
+use std::fs;
+
 fn main() {
+    let pass = "a";
+    let salt = [0u8; 32];
+    let key = derive_key_256(pass.as_bytes(), &salt);
+    println!("{:?}", key);
+
+    let input = fs::read(PathBuf::from("test.axx")).unwrap();
+    let (guid, input) = slice_guid(&input);
+    println!("guid: {:?}, input: {:?}", guid, input);
+
     println!("Hello, world!");
+}
+
+fn derive_key_256(pass: &[u8], salt: &[u8]) -> [u8; 32] {
+    let sha1 = AxxSha1::new();
+    let mut key = [0u8; 32];
+    let mut hmac = Hmac::new(sha1, pass);
+    pbkdf2(&mut hmac, &salt, 48000, &mut key);
+    key
+}
+
+struct AxxSha1 {
+    sha1: Sha1
+}
+
+impl AxxSha1 {
+    pub fn new() -> AxxSha1 {
+        AxxSha1 {
+            sha1: Sha1::new()
+        }
+    }
+}
+
+impl Digest for AxxSha1 {
+    fn input(&mut self, input: &[u8]) {
+        self.sha1.input(input)
+    }
+
+    fn result(&mut self, out: &mut [u8]) {
+        self.sha1.result(out)
+    }
+
+    fn reset(&mut self) {
+        self.sha1.reset()
+    }
+
+    fn output_bits(&self) -> usize {
+        self.sha1.output_bits()
+    }
+
+    fn block_size(&self) -> usize {
+        20
+    }
+}
+
+fn slice_guid(input: &[u8]) -> (&[u8], &[u8]) {
+    input.split_at(16)
 }
