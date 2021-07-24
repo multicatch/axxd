bae02516c43798b2885ef6cd0cb4bdea56bcd068
multicatch <cezaryre@gmail.com>
2021-07-24 16:06:18 +0200

    Added master key extraction

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use crypto::pbkdf2::pbkdf2;
-use crypto::hmac::Hmac;
 use crypto::sha1::Sha1;
 use crypto::digest::Digest;
 use std::path::PathBuf;
@@ -15,8 +13,7 @@ use axxd::key::{KeyParams, HeaderDecryptor};
 
 fn main() {
     let pass = "a";
-    let salt = [0u8; 32];
-    let key = derive_key_256(pass.as_bytes(), &salt);
+    let key = derive_key(pass.as_bytes());
     println!("{:?}", key);
 
     let input = fs::read(PathBuf::from("test.axx")).unwrap();
@@ -51,44 +48,12 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn derive_key_256(pass: &[u8], salt: &[u8]) -> [u8; 32] {
-    let sha1 = AxxSha1::new();
-    let mut key = [0u8; 32];
-    let mut hmac = Hmac::new(sha1, pass);
-    pbkdf2(&mut hmac, &salt, 48000, &mut key);
-    key
-}
-
-struct AxxSha1 {
-    sha1: Sha1
-}
-
-impl AxxSha1 {
-    pub fn new() -> AxxSha1 {
-        AxxSha1 {
-            sha1: Sha1::new()
-        }
-    }
-}
-
-impl Digest for AxxSha1 {
-    fn input(&mut self, input: &[u8]) {
-        self.sha1.input(input)
-    }
-
-    fn result(&mut self, out: &mut [u8]) {
-        self.sha1.result(out)
-    }
-
-    fn reset(&mut self) {
-        self.sha1.reset()
-    }
-
-    fn output_bits(&self) -> usize {
-        self.sha1.output_bits()
-    }
-
-    fn block_size(&self) -> usize {
-        20
-    }
+fn derive_key(pass: &[u8]) -> [u8; 16] {
+    let mut key = [0; 20];
+    let mut sha1 = Sha1::new();
+    sha1.input(pass);
+    sha1.result(&mut key);
+    let mut result: [u8; 16] = Default::default();
+    result.copy_from_slice(&key[0..16]);
+    result
 }
