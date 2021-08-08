b50352c409bbf9326605555511584ff4b1d445e7
multicatch <cezaryre@gmail.com>
2021-08-08 00:02:56 +0000

    Deploying to gh-pages from @ multicatch/axxd@22c89abd9dfa694859df27604499e93481fce0b4 🚀

--- implementors/core/hash/trait.Hash.js
+++ implementors/core/hash/trait.Hash.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["axxd"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"axxd/content/enum.HeaderBlockType.html\" title=\"enum axxd::content::HeaderBlockType\">HeaderBlockType</a>","synthetic":false,"types":["axxd::content::HeaderBlockType"]}];
+implementors["axxd"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"enum\" href=\"axxd/content/enum.HeaderBlockType.html\" title=\"enum axxd::content::HeaderBlockType\">HeaderBlockType</a>","synthetic":false,"types":["axxd::content::HeaderBlockType"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
